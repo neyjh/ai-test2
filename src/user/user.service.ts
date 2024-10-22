73f52c18a3892a74b97dad48571954ff239bccf5
neyjh <ney@jinhakapply.com>
2024-10-22 21:36:02 +0900

    feat: user 모듈 생성

---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+// src/user/user.service.ts
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async register(email: string, password: string, name: string): Promise<User> {
+    const existingUser = await this.userRepository.findOne({ where: { email } });
+    if (existingUser) {
+      throw new ConflictException('이미 존재하는 이메일입니다.');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = this.userRepository.create({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    return this.userRepository.save(newUser);
+  }
+
+  async unregister(userId: number): Promise<void> {
+    const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다.');
+    }
+
+    await this.userRepository.remove(user);
+  }
+}
